421715f3cfb3a56150070a4271f56721757f708a
Manh Cuong <cuong@radioactive.sg>
2023-04-12 17:42:45 +0800

    save previous question

--- backend/backend/api.py
+++ backend/backend/api.py
@@ -18,31 +18,24 @@ def getResult(request):
     choice = 2
 
     question = post_data.get("text", "")
-    print(post_data)
-    print(question)
+    preInput = post_data.get("preInput", "")
+    preResponse = post_data.get("preResponse", "")
+    messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": preInput},
+            {"role": "assistant", "content": preResponse},
+            {"role": "user", "content": question}
+        ]
+    print(messages)
 
     if choice == 1:
         return HttpResponse(question)
-    response = openai.Completion.create(
-        model="text-davinci-002",
-        prompt=question,
-        temperature=0,
-        max_tokens=100,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        stop=["\n"]
-    )
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            # {"role": "system", "content": "You are a helpful assistant."},
-            # {"role": "user", "content": "Who won the world series in 2020?"},
-            # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            {"role": "user", "content": question}
-        ]
+        model = "gpt-3.5-turbo",
+        messages = messages
     )
     if response:
         answer = response["choices"][0]["message"]["content"]
+        
     print(answer)
     return HttpResponse(answer)
